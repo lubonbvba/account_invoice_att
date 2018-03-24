9173a97a1c1ab9a959a243088abc95d6085a983e
Luc Bonjean <luc@lubon.be>
2018-03-24 14:36:34 +0100

    Initial version

--- models/mail_compose.py
+++ models/mail_compose.py
@@ -8,20 +8,6 @@ import pdb
 class mail_compose_message(models.Model):
     _inherit = 'mail.compose.message'
 
-    @api.multi
-    def zzzsend_mail(self):
-        context = self._context
-        if context.get('default_model') == 'account.invoice' and \
-                context.get('default_res_id') and context.get('mark_invoice_as_sent'):
- #           invoice = self.env['account.invoice'].browse(context['default_res_id'])
- #           invoice = invoice.with_context(mail_post_autofollow=True)
- #           invoice.write({'sent': True})
- #           invoice.message_post(body=_("Invoice sent"))
-            company=self.env['account.invoice'].browse(context['default_res_id']).company_id
-            for att in company.default_attachments:
-                self.attachment_ids=[(4,att.id)]
-        return super(mail_compose_message, self).send_mail()	
-
     @api.model
     def get_mail_values(self, wizard, res_ids):
         context = self._context
